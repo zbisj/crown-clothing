d6544289f9df41ec518a71a5ac484020072275df
Sibabale <sibabale@wealthmigrate.com>
2019-09-28 23:25:06 +0200

    added the header

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css'
 import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shop/ShopPage';
+import Header from './components/header/Header';
 
 function App() {
+
   return (
+
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={ HomePage } />
         <Route path="/shop" component={ ShopPage } />
       </Switch>
     </div>
   );
-}
+
+};
 
 export default App;
