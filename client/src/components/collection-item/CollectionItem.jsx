7542ea956068c1c9c66f99a5e49dee80598d65da
sibabale <jojasibabale@gmail.com>
2021-06-02 09:11:20 +0200

    added a dollar sign next to price

--- client/src/components/collection-item/CollectionItem.jsx
+++ client/src/components/collection-item/CollectionItem.jsx
@@ -1,43 +1,35 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
-import { addItem } from '../../redux/actions/CartActions';
+import { addItem } from "../../redux/actions/CartActions";
 import {
-  Item, AddToCartButton, Footer, NameContainer, PriceContainer
-} from './collection-item.styles';
+  Item,
+  AddToCartButton,
+  Footer,
+  NameContainer,
+  PriceContainer,
+} from "./collection-item.styles";
 
-const CollectionItem = ({item, addItem}) => {
-
+const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
 
   return (
-
     <Item>
-
-      <div
-        className="image"
-        style={{backgroundImage: `url(${ imageUrl })`}}
-      />
+      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
 
       <Footer>
-        <NameContainer>{ name }</NameContainer>
-        <PriceContainer>{ price }</PriceContainer>
+        <NameContainer>{name}</NameContainer>
+        <PriceContainer>${price}</PriceContainer>
       </Footer>
-      <AddToCartButton
-        inverted
-        onClick={ () => addItem(item) }
-      >
+      <AddToCartButton inverted onClick={() => addItem(item)}>
         ADD TO CART
       </AddToCartButton>
-
     </Item>
-
   );
-
 };
 
-const mapDispatchToProps = dispatch => ({
-  addItem: item => dispatch(addItem(item))
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
 });
 
 export default connect(null, mapDispatchToProps)(CollectionItem);
