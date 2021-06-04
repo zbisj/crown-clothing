e9b730583b54be8fe696191e9e4fdfd1cba7b62d
sibabale <jojasibabale@gmail.com>
2021-06-04 09:41:38 +0200

    formatting changes by prettier

--- client/src/pages/collections/CollectionPage.jsx
+++ client/src/pages/collections/CollectionPage.jsx
@@ -1,32 +1,30 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
-import './collection-page.styles.scss';
+import "./collection-page.styles.scss";
 
-import CollectionItem from '../../components/collection-item/CollectionItem';
-import { selectCollection } from '../../redux/selectors/ShopSelectors';
+import CollectionItem from "../../components/collection-item/CollectionItem";
+import { selectCollection } from "../../redux/selectors/ShopSelectors";
 
 const CollectionPage = ({ collection }) => {
+  console.log(collection);
+
   const { title, items } = collection;
 
   return (
     <div className="collection-page">
-      <h2 className="title">{ title }</h2>
+      <h2 className="title">{title}</h2>
       <div className="items">
-        {
-          items.map(item => <CollectionItem key={ item.id } item={ item }/>)
-        }
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
       </div>
     </div>
-  )
-
+  );
 };
 
-const mapStateToProps = (state, ownProps) => (
-  {
-    collection: selectCollection(ownProps.match.params.collectionId)(state)
-  }
-
-)
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
 
 export default connect(mapStateToProps)(CollectionPage);
